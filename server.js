405568a727681bcb1dc84290e093419a652aa7aa
Erfan Soleimani <erfansoleimani000@gmail.com>
2025-07-11 15:51:08 +0330

    CORS fix

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
